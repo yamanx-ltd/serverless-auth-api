c3db0b241dbf50244c3ac91013b9016d1deef68b
Fehmi Anaç <fehmianac@gmail.com>
2023-10-16 15:20:58 +0200

    added update email & phone

---
+++ src/Api/Endpoints/V1/Phone/Put.cs
@@ -0,0 +1,31 @@
+using Api.Infrastructure.Contract;
+using Api.Infrastructure.Extensions;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Endpoints.V1.Phone;
+
+public class Put : IEndpoint
+{
+    private static async Task<IResult> Handler(
+        [FromRoute] string userId,
+        [FromBody] UpdateUserPhoneMappingRequest request,
+        [FromServices] IAuthService authService,
+        CancellationToken cancellationToken)
+    {
+        await authService.UpdateUserPhoneMappingAsync(userId, request.Phone, cancellationToken);
+        return Results.Ok();
+    }
+
+    public RouteHandlerBuilder MapEndpoint(IEndpointRouteBuilder endpoints)
+    {
+        return endpoints.MapPut("/v1/users/{userId}/phone", Handler)
+            .Produces200()
+            .Produces400()
+            .Produces404()
+            .Produces500()
+            .WithTags("User");
+    }
+
+    public record UpdateUserPhoneMappingRequest(string Phone);
+}
