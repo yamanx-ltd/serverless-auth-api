4f36c2adad020398160cd853a780661ab0cb04b5
Fehmi Anaç <fehmianac@gmail.com>
2024-04-12 19:09:07 +0300

    Logout endpoint added.

---
+++ src/Api/Endpoints/V1/Logout/Post.cs
@@ -0,0 +1,31 @@
+using Api.Infrastructure.Contract;
+using Api.Infrastructure.Extensions;
+using Domain.Services;
+using Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Endpoints.V1.Logout;
+
+public class Post : IEndpoint
+{
+    private static async Task<IResult> Handler(
+        [FromBody] LogoutRequest request,
+        [FromServices] IApiContext apiContext,
+        [FromServices] IAuthService authService,
+        [FromServices] IEventBusManager eventBusManager,
+        CancellationToken cancellationToken)
+    {
+        await authService.DeleteRefreshTokenAsync(request.RefreshToken, cancellationToken);
+        return Results.Ok();
+    }
+
+    public RouteHandlerBuilder MapEndpoint(IEndpointRouteBuilder endpoints)
+    {
+        return endpoints.MapPost("v1/logout", Handler)
+            .Produces200()
+            .Produces400()
+            .WithTags("User");
+    }
+
+    public record LogoutRequest(string RefreshToken, string? DeviceToken);
+}
